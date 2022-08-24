62a67bb52993d66d20695d766d10d446aa8bc82a
Danilo Argolo <dan_argolo@hotmail.com>
2022-08-24 12:00:11 -0300

    Resolvendo o 2º exercicio 4.3

--- fundamentos/bloco-04-introdução-javascript/4.3-exercicio.js
+++ fundamentos/bloco-04-introdução-javascript/4.3-exercicio.js
@@ -11,3 +11,16 @@ for (let r = 1; r <= num; r += 1){
 }
 
 console.log(fatorial)
+
+// --- 2 ---
+// Desenvolva um algoritmo que é capaz de inverter uma palavra.
+
+let word = "melancia"
+
+let split = word.split("");
+
+let invert = split.reverse();
+
+let join = invert.join("")
+
+console.log(join);
